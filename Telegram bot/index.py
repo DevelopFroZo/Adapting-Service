52f98552567ee6c9c2f28c3fd508e7734d0d8871
FroZo <frozo02@yandex.ru>
2019-09-06 01:01:47 +0800

    Bot testing

--- Telegram bot/index.py
+++ Telegram bot/index.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import apihelper
+from random import randint
 
 f = open( 'token.txt', 'r' )
 token = f.readline()[ : -1 ]
@@ -14,8 +15,29 @@ telebot.apihelper.proxy = {
 bot = telebot.TeleBot( token )
 
 @bot.message_handler( commands = [ 'start' ] )
-def test( message ):
-  bot.send_message( message.chat.id, 'hello' )
+def start( message ):
+  bot.send_message( message.chat.id, 'Hello, this bot can do nothing. Type /help' )
+
+@bot.message_handler( commands = [ 'help' ] )
+def help( message ):
+  bot.send_message( message.chat.id, 'Yeah. For calculate two numbers type\ncalc %num% %num% %sign%\nWhere sign is: +, -, /, *' )
+
+@bot.message_handler( content_types = [ 'text' ] )
+def calc( message ):
+  print( '{}: {}'.format( message.from_user.username, message.text ) )
+
+  tmp = str( message.text ).split( ' ' )
+  cmd = tmp[0]
+
+  if cmd.lower() != 'calc':
+    bot.send_message( message.chat.id, 'ну ты мда' )
+
+    return
+
+  num0 = int( tmp[1] )
+  num1 = int( tmp[2] )
+  res = num0 + num1
+  bot.send_message( message.chat.id, '{}'.format( res ) )
 
 print( 'Try to run bot with proxy {}'.format( proxyUrl ) )
 bot.polling()
