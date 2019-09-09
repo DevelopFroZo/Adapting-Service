6000229304da6651632fca67e287f6a4432fb531
FroZo <frozo02@yandex.ru>
2019-09-09 23:51:32 +0800

    Success testing

--- public/js/sendTest.js
+++ public/js/sendTest.js
@@ -1,5 +1,44 @@
 function sendTest(){
-  console.log( saveClick() );
+  let requests, cookie, testData;
+
+  requests = new Requests( {
+    dataType : "json",
+    responsePreprocess : data => JSON.parse( data )
+  } );
+  cookie = new Cookie();
+  testData = saveClick();
+
+  requests.post(
+    "/companies/authorize",
+    {
+      email : "example@example.com",
+      password : "123456"
+    }
+  )
+  .then( data => {
+    if( !data ){
+      console.log( data.message );
+
+      return;
+    }
+
+    cookie.set( "token", data.token );
+    console.log( `Authorized ${data.token}` );
+
+    return requests.post(
+      "/tests/add",
+      testData
+    );
+  } )
+  .then( console.log )
+  .catch( console.log );
+
+  /*requests.post(
+    "/tests/add",
+    testData
+  )
+  .then( console.log )
+  .catch( console.log );*/
 }
 
 function index(){
