b836b280197e601a5725504a5243642271fbc51e
vpasport <pasportnikov2000@gmail.com>
2019-09-11 00:49:58 +0800

    database stucture created

---
+++ Telegram bot/database/connectSettings.py
@@ -0,0 +1,8 @@
+def getConnectSettings():
+    return {
+        'user' : 'adaptingservice',
+        'password' : 'td354325tr432543',
+        'host' : 'localhost',
+        'port' : 5432,
+        'database' : 'adaptingservice'
+    }
