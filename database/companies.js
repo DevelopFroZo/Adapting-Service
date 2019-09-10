396cf4c93692d857b7ba1050d1448dd45c469aa7
FroZo <frozo02@yandex.ru>
2019-09-11 03:15:59 +0800

    Fix errors -> create base class for all database modules that support new interface for errors -> fatal or not, smart trace and process errors. Fix sql scripts -> tables add correct

--- database/companies.js
+++ database/companies.js
@@ -1,23 +1,24 @@
-let crypto;
+let BaseDatabaseModule, crypto;
 
+BaseDatabaseModule = require( "./baseDatabaseModule" );
 crypto = require( "crypto" );
 
-class Companies{
+class Companies extends BaseDatabaseModule{
   constructor( modules ){
-    this.modules = modules;
+    super( modules, "Companies" );
   }
 
   authorize( authData ){
     let token;
 
-    return new Promise( ( res, rej ) => this.modules.db.query(
+    return super.promise( ( success, error, fatal ) => this.modules.db.query(
       "select password, token " +
       "from companies " +
       "where email = $1",
       [ authData.email ]
     )
     .then( data => {
-      if( data.rowCount === 0 ) rej( `Email "${authData.email}" не найден` );
+      if( data.rowCount === 0 ) error( { error : `Email "${authData.email}" не найден` } );
 
       return data;
     } )
@@ -27,16 +28,13 @@ class Companies{
       password = data.rows[0].password.split( ";" );
       authData.password = crypto.createHash( "sha1" ).update( `${authData.password}${password[1]}` ).digest( "hex" );
 
-      if( authData.password !== password[0] ) rej( "Неверный пароль" );
+      if( authData.password !== password[0] ) error( { error : "Неверный пароль" } );
 
       return data;
     } )
     .then( data => {
       if( data.rows[0].token !== null ){
-        res( {
-          isSuccess : true,
-          token : data.rows[0].token
-        } );
+        success( { token : data.rows[0].token } );
 
         return;
       }
@@ -50,25 +48,22 @@ class Companies{
         [ token, authData.email ]
       );
     } )
-    .then( () => res( {
-      isSuccess : true,
-      token
-    } ) )
-    .catch( rej ) );
+    .then( () => success( { token } ) )
+    .catch( error => fatal( error, "authorize" ) ) );
   }
 
   isTokenValid( token ){
-    return new Promise( ( res, rej ) => this.modules.db.query(
+    return super.promise( ( success, error, fatal ) => this.modules.db.query(
       "select id " +
       "from companies " +
       "where token = $1",
       [ token ]
     )
     .then( data => {
-      if( data.rowCount === 0 ) res( false );
-      else res( true );
+      if( data.rowCount === 0 ) success( { isValid : false } );
+      else error( { isValid : true } );
     } )
-    .catch( rej ) );
+    .catch( error => fatal( error, "isTokenValid" ) ) );
   }
 }
 
