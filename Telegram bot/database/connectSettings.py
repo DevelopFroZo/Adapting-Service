294a6c5b8776e3c1ac8197dfe24db6fd469eae03
vpasport <pasportnikov2000@gmail.com>
2019-09-16 22:18:52 +0800

    fix merge conflict

--- Telegram bot/database/connectSettings.py
+++
@@ -1,8 +0,0 @@
-def getConnectSettings():
-    return {
-        'user' : 'adaptingservice',
-        'password' : 'td354325tr432543',
-        'host' : 'localhost',
-        'port' : 5432,
-        'database' : 'adaptingservice'
-    }
