7ec48fa310ee5227354e6a5bf779935bb2c223a8
Max Zhukov <promakc31@gmail.com>
2019-10-18 22:16:36 +0800

    add notifications for users. Now user can delete and add new workers

--- public/js/cabinet.js
+++ public/js/cabinet.js
@@ -58,7 +58,10 @@ async function initTests() {
 }
 
 async function initUsers() {
+
     let workersInfo = await getWorkers();
+
+    let userBlock = $(".user-deleteble").html();
 
     if (workersInfo.ok) {
         let workers = workersInfo.data;
@@ -66,35 +69,70 @@ async function initUsers() {
         $(".user-length").hide();
 
         for (let i = 0; i < workers.length; i++) {
-            let block = $("<div/>").addClass("user-info active-hover").html($(".user-info").html());
-            block.find(".user-name").text(workers[i].name);
-            block.find(".user-code").text(workers[i].key)
-            $(".users").append(block)
-
-            block.find(".user-more").on("click", function () {
-                block.children(".hoverable").addClass("user-active");
-                $(".user-info:not(.user-active)").removeClass("active-hover");
-                block.children(".user-buttons-block").css({
-                    opacity: 1,
-                    visibility: "visible"
-                })
-            })
+            showUser(workers[i].name, workers[i].key, workers[i].id);
         }
+    }
+    else if (workersInfo.code === 8) {
+        $(".user-length").text("Добавьте сотрудника кнопкой сверху")
+    }
 
-        $(document).mouseup(function (e) { // событие клика по веб-документу
-            var div = $(".user-buttons-block"); // тут указываем ID элемента
-            if (!div.is(e.target) // если клик был не по нашему блоку
-                && div.has(e.target).length === 0) { // и не по его дочерним элементам
-                div.css({
-                    visibility: "hidden",
-                    opacity: "0"
-                });
+    $(document).mouseup(function (e) { // событие клика по веб-документу
+        var div = $(".user-buttons-block"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            && div.has(e.target).length === 0) { // и не по его дочерним элементам
+            div.css({
+                visibility: "hidden",
+                opacity: "0"
+            });
+            $(".user-active").removeClass("user-active");
+            $(".user-info").addClass("active-hover")
+        }
+    });
+
+    function showUser(name, key, id) {
+        let block = $("<div/>").addClass("user-info active-hover").html(userBlock);
+        block.find(".user-name").text(name);
+        block.find(".user-code").text(key);
+
+        block.find(".delete-user-button").on("click", async function () {
+            let deleteStatus = await deleteWorker(id);
+
+            if (deleteStatus.ok) {
+                block.remove();
+
                 $(".user-active").removeClass("user-active");
-                $(".user-info").addClass("active-hover")
-            }
-        });
+                $(".user-info").addClass("active-hover");
 
-        $("#search-user").on("keyup", function () {
+                if ($(".user-info").length === 0)
+                    $(".user-length").text("Добавьте сотрудника кнопкой сверху").show();
+            }
+            else
+                showMessage("error-message", deleteStatus.description)
+        })
+
+        $(".users").append(block)
+
+        block.find(".user-more").on("click", function () {
+            block.children(".hoverable").addClass("user-active");
+            $(".user-info:not(.user-active)").removeClass("active-hover");
+            block.children(".user-buttons-block").css({
+                opacity: 1,
+                visibility: "visible"
+            })
+        })
+
+        $(".user-length").hide();
+    }
+
+    function hideAddUser() {
+        $(".add-user-block").hide();
+        $("#add-user-name").val("");
+        $("#add-user").removeAttr("disabled");
+        $("#save-new-user").attr("disabled", "disabled");
+    }
+
+    $("#search-user").on("keyup", function () {
+        if ($(".user-info").length > 0) {
             let name = $(".user-name")
             for (let i = 0; i < name.length; i++) {
                 if (name.eq(i).text().toLowerCase().includes($(this).val().toLowerCase()))
@@ -106,11 +144,40 @@ async function initUsers() {
                 $(".user-length").text("Пользователь не найден").show();
             else
                 $(".user-length").hide();
-        })
-    }
-    else if (workersInfo.code === 8) {
-        $(".user-length").text("Добавьте сотрудника кнопкой сверху")
-    }
+        }
+    })
+
+    $("#add-user").on("click", function () {
+        $(".add-user-block").css("display", "flex");
+        $(this).attr("disabled", "disabled");
+        $(".user-length").hide();
+    })
+
+    $("#save-new-user").on("click", async function () {
+        let userStatus = await addWorker($("#add-user-name").val());
+
+        if (userStatus.ok) {
+            showUser($("#add-user-name").val(), userStatus.data.code, userStatus.data.id);
+
+            hideAddUser();
+        }
+        else
+            showMessage("error-message", userStatus.description);
+    })
+
+    $("#add-user-name").on("keyup", function () {
+        if ($(this).val().length > 0)
+            $("#save-new-user").removeAttr("disabled");
+        else
+            $("#save-new-user").attr("disabled", "disabled")
+    })
+
+    $("#close-add-user").on("click", function () {
+        hideAddUser();
+
+        if ($(".user-info").length === 0)
+            $(".user-length").show();
+    })
 
     $(".user-deleteble").remove();
 }
