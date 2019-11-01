8414199d5dbcaf097d5cc4b1202d6fc660db82d7
vpasport <pasportnikov2000@gmail.com>
2019-11-01 17:36:12 +0800

    fixed question and answer

--- Telegram bot/bot.py
+++ Telegram bot/bot.py
@@ -28,12 +28,45 @@ def post( path, data ):
         connectSettings[ 'databaseIp' ],
         path
     ),
-     json=data ).json()
+    json=data )
 
-    print( '\n\n\n' + path )
-    print( response )
+    try:
+        response = response.json()
+        
+        print( '\n\n\n' + path )
+        print( response )
 
-    return response
+        return response
+    except requests.exceptions.Timeout as e:
+        print( 'RESPONSE TIMEOUT ERROR!!!\n{}'.format( e ) )
+    except requests.exceptions.RequestException as e:
+        print( 'RESPONSE ERROR!!!:\n{}'.format( e ) )
+
+
+def sendMessage( id, message, reply_markup=None, parse_mode=None ):
+    try:
+        msg = bot.send_message( id, message, reply_markup=reply_markup, parse_mode=parse_mode )
+        return msg
+    except telebot.apihelper.ApiException as e:
+        result_dict = json.loads(e.result.text) 
+        description = result_dict['description'] 
+        if description == "bot was blocked by the user": 
+            raise 
+        else: 
+            print("err")
+            pass
+        return None
+
+
+def editMessage( chat_id, message_id, text, reply_markup, parse_mode ):
+    # print( text )
+    try:
+        msg = bot.edit_message_text( chat_id=chat_id, message_id=message_id, text=text, reply_markup=reply_markup, parse_mode=parse_mode )
+    # bot.edit_message_reply_markup( chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=keyboard )
+        return msg
+    except telebot.apihelper.ApiException as e:
+        print( 'ERROR!!!: \n{}'.format( e ) )
+        return None
 
 
 # Bot /start command handler
@@ -43,26 +76,26 @@ def start( message ):
     keyboard.row( '/auth' )
     keyboard.row( '/help' )
 
-    bot.send_message( message.chat.id, messages[ 'greeting' ] )
-    bot.send_message( message.chat.id, messages[ 'info' ], reply_markup=keyboard )
+    sendMessage( message.chat.id, messages[ 'greeting' ] )
+    sendMessage( message.chat.id, messages[ 'info' ], reply_markup=keyboard )
 
 
 # Bot /help command handler
 @bot.message_handler( commands = [ 'help' ] )
 def help( message ):
-    bot.send_message( message.chat.id, messages[ 'help' ] )
+    sendMessage( message.chat.id, messages[ 'help' ] )
 
 
 # Bot /auth command handler
 @bot.message_handler( commands = [ 'auth' ] )
 def auth( message ):
-    msg = bot.send_message( message.chat.id, messages[ 'auth' ] )
+    msg = sendMessage( message.chat.id, messages[ 'auth' ] )
     bot.register_next_step_handler( msg, authHandler )
 
 
 def authHandler( message ):
     if message.text.lower() == 'отмена':
-        bot.send_message( message.chat.id, 'Авторизация отменена' )
+        sendMessage( message.chat.id, 'Авторизация отменена' )
         return False
     
     msg = message.text.split( ',' )
@@ -75,14 +108,14 @@ def authHandler( message ):
             'telegramId' : message.chat.id
         } )
     else:
-        msg = bot.send_message( message.chat.id, messages[ 'incorrectAuthData' ], reply_markup=keyboard )
+        msg = sendMessage( message.chat.id, messages[ 'incorrectAuthData' ], reply_markup=keyboard )
         bot.register_next_step_handler( msg, authHandler )
         return
 
     if not response[ 'ok' ]:
         if response[ 'code' ] != 5:
             keyboard.row( 'Отмена' )
-            msg = bot.send_message( message.chat.id, '{}, повторите попытку или напишите "Отмена"'.format(
+            msg = sendMessage( message.chat.id, '{}, повторите попытку или напишите "Отмена"'.format(
                 codeHandler( response )
             ), reply_markup=keyboard )
 
@@ -91,14 +124,14 @@ def authHandler( message ):
             keyboard.row( '/info' )
             keyboard.row( '/help' )
 
-            bot.send_message( message.chat.id, codeHandler( response ), reply_markup=keyboard )
+            sendMessage( message.chat.id, codeHandler( response ), reply_markup=keyboard )
     else:
         keyboard.row( '/info' )
         keyboard.row( '/help' )
 
-        bot.send_message( message.chat.id, messages[ 'successAuth' ].format(
+        sendMessage( message.chat.id, messages[ 'successAuth' ].format(
             response[ 'data' ]
-        ), reply_markup=keyboard )
+        ), reply_markup=keyboard, parse_mode="Markdown" )
 
 
 # Bot /info commands handler
@@ -112,13 +145,13 @@ def infoBlock( message ):
     } )
 
     if not response[ 'ok' ]: 
-        bot.send_message( message.chat.id, codeHandler( response ) )
+        sendMessage( message.chat.id, codeHandler( response ) )
     else:
-        bot.send_message( message.chat.id, 'Тема: {}\n\n{}\n\n{}'.format(
+        sendMessage( message.chat.id, 'Тема: {}\n\n{}\n\n{}'.format(
             response[ 'data' ][ 'name' ],
             response[ 'data' ][ 'description' ],
-            messages[ 'infoStartTest' ]
-        ), reply_markup=keyboard )
+            messages[ 'infoStartTest' ] 
+        ), reply_markup=keyboard, parse_mode="Markdown" )
 
 
 # Bot /startTest commands handler
@@ -135,32 +168,32 @@ def printTestMessage( message ):
     
     if response[ 'data' ][ 'question' ][ 'type' ] == 'short':
         text = printTest( message, response )
-        msg = bot.send_message( message.chat.id, '{}\n\n{}'.format(
+        msg = sendMessage( message.chat.id, '{}\n\n{}'.format(
             text,
             messages[ 'answerInfoShort' ]
-        ) )
+        ), parse_mode="Markdown" )
         bot.register_next_step_handler( msg, sendShortOrLongAnswer )
     elif response[ 'data' ][ 'question' ][ 'type' ] == 'long':
         text = printTest( message, response )
-        msg = bot.send_message( message.chat.id, '{}\n\n{}'.format(
+        msg = sendMessage( message.chat.id, '{}\n\n{}'.format(
             text,
             messages[ 'answerInfoLong' ]
-        ) )
+        ), parse_mode="Markdown" )
         bot.register_next_step_handler( msg, sendShortOrLongAnswer )
     elif response[ 'data' ][ 'question' ][ 'type' ] == 'many variant':
         keyboard = createManyVariantKeyboard( response )
         text = printTestVariant( message, response )
-        msg = bot.send_message( message.chat.id, '{}\n\n{}'.format(
+        msg = sendMessage( message.chat.id, '{}\n\n{}'.format(
             text,
             messages[ 'answerInfoManyVariant' ]
-        ), reply_markup=keyboard )
+        ), reply_markup=keyboard, parse_mode="Markdown" )
     elif response[ 'data' ][ 'question' ][ 'type' ] == 'one variant':
         keyboard = createOneVariantKeyboard( response )
         text = printTestVariant( message, response )
-        msg = bot.send_message( message.chat.id, '{}\n\n{}'.format(
+        msg = sendMessage( message.chat.id, '{}\n\n{}'.format(
             text,
             messages[ 'answerInfoOneVariant' ]
-        ), reply_markup=keyboard )
+        ), reply_markup=keyboard, parse_mode="Markdown" )
 
 def getTestFromdb( message ):
     response = post( 'getQuestion', {
@@ -170,15 +203,15 @@ def getTestFromdb( message ):
     keyboard = telebot.types.ReplyKeyboardMarkup( resize_keyboard=True, one_time_keyboard=True )
 
     if not response[ 'ok' ]:
-        bot.send_message( message.chat.id, codeHandler( response ) )
+        sendMessage( message.chat.id, codeHandler( response ) )
         return False
     else:
         if response[ 'code' ] == 4:
             keyboard.row( '/info' )
-            bot.send_message( message.chat.id, codeHandler( response ) )
+            sendMessage( message.chat.id, codeHandler( response ), reply_markup=keyboard )
             return False
         elif response[ 'code' ] == 7:
-            bot.send_message( message.chat.id, codeHandler( response ) )
+            sendMessage( message.chat.id, codeHandler( response ) )
 
     post( 'acceptQuestion', {
         'telegramId' : message.chat.id
@@ -188,10 +221,10 @@ def getTestFromdb( message ):
 
 
 def printTest( message, test ):
-    return '{}) {}\n\nНа ответ отведено {} минут'.format(
+    return '{}) {}\n\n⏳*{}*⏳'.format(
         test[ 'data' ][ 'question' ][ 'number' ],
         test[ 'data' ][ 'question' ][ 'description' ],
-        test[ 'data' ][ 'question' ][ 'time' ]
+        getTime( test[ 'data' ][ 'question' ][ 'time' ] )
     )
 
 
@@ -204,14 +237,26 @@ def printTestVariant( message, test ):
             test[ 'data' ][ 'possibleAnswers' ][ i ][ 'description' ]
         )
 
-    question = '{}) {}\n\n{}\n\nНа ответ отведено {} минут'.format(
+    question = '{}) {}\n\n{}\n⏳*{}*⏳'.format(
         test[ 'data' ][ 'question' ][ 'number' ],
         test[ 'data' ][ 'question' ][ 'description' ],
         answer,
-        test[ 'data' ][ 'question' ][ 'time' ]
+        getTime( test[ 'data' ][ 'question' ][ 'time' ] )
     )
 
     return question
+
+
+def getTime( time ):
+    e = int( time ) % 10
+    if int( time ) <= 20 & e > 4:
+        return 'На ответ отведено {} минут'.format( time )
+    if e == 1:
+        return 'На ответ отведена {} минута'.format( time )
+    if e > 1 & e < 5:
+        return 'На ответ отведено {} минуты'.format( time )
+    if e > 4:
+        return 'На ответ отведено {} минут'.format( time )
 
 
 def createManyVariantKeyboard( test ):
@@ -261,8 +306,12 @@ def createOneVariantKeyboard( test ):
 @bot.callback_query_handler( func=lambda call: True )
 def callback_inline( call ):
     data = call.message.json[ 'reply_markup' ]
+
+    if data == None:
+        return False
+
     keyboard = telebot.types.InlineKeyboardMarkup()
-    
+        
     isOneVariant = True
 
     for elem in data[ 'inline_keyboard' ]:
@@ -279,9 +328,9 @@ def callback_inline( call ):
                     if el[ 'callback_data' ] != 'send':
                         if int( el[ 'callback_data' ] ) < 0:
                             answer.append( ( int( el[ 'callback_data' ] ) * -1 ) )
-            
+                
             if len( answer ) == 1:
-                bot.send_message( call.message.chat.id, 'Вы выбрали только один ответ, выберите ещё' )
+                sendMessage( call.message.chat.id, 'Вы выбрали только один ответ, выберите ещё' )
                 return False
 
             response = post( 'sendVariantAnswer', {
@@ -289,14 +338,14 @@ def callback_inline( call ):
                 'possibleAnswerIds' : answer
             } )
 
-            if not response[ 'ok' ]:
-                bot.send_message( call.message.chat.id, codeHandler( response ) )
-            else: 
-                bot.send_message( call.message.chat.id, codeHandler( response ) )
-            bot.edit_message_text( chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text, reply_markup=keyboard )
+            sendMessage( call.message.chat.id, codeHandler( response ) )
+
+            text = editText( call.message.text )
+
+            editMessage( chat_id=call.message.chat.id, message_id=call.message.message_id, text=text, reply_markup=keyboard, parse_mode="Markdown" )
             printTestMessage( call.message )
             return True
-        
+            
         button = []
 
         for elem in data[ 'inline_keyboard' ]:
@@ -311,27 +360,39 @@ def callback_inline( call ):
                     callbackData = el[ 'callback_data' ]
                     but = telebot.types.InlineKeyboardButton( text=el[ 'text' ], callback_data=callbackData )
                 button.append( but )
-        
+            
             if len( button ) > 1:
                 keyboard.row( button[0], button[1] )
             else:
                 keyboard.row( button[0] )
             button = []
 
-        bot.edit_message_text( chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text, reply_markup=keyboard )
-    else:
-        response = post( 'sendVariantAnswer', {
-            'telegramId' : call.message.chat.id,
-            'possibleAnswerIds' :  ( int( call.data ), )
-        } )
+        text = editText( call.message.text )
 
-        if not response[ 'ok' ]:
-            bot.send_message( call.message.chat.id, codeHandler( response ) )
-        else: 
-            bot.send_message( call.message.chat.id, codeHandler( response ) )
-        bot.edit_message_text( chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text, reply_markup=keyboard )
-        printTestMessage( call.message )
-        return True
+        editMessage( chat_id=call.message.chat.id, message_id=call.message.message_id, text=text, reply_markup=keyboard, parse_mode="Markdown" )
+    else:
+        text = editText( call.message.text )
+
+        msg = editMessage( chat_id=call.message.chat.id, message_id=call.message.message_id, text=text, reply_markup=keyboard, parse_mode="Markdown" )
+      
+        if msg != None:
+            response = post( 'sendVariantAnswer', {
+                'telegramId' : call.message.chat.id,
+                'possibleAnswerIds' :  ( int( call.data ), )
+            } )
+            
+            sendMessage( call.message.chat.id, codeHandler( response ) )
+            printTestMessage( call.message )
+            return True
+        else:
+            return False
+
+
+def editText( text ):
+    text = text.split( '⏳' )
+    text[1] = '⏳*{}*⏳'.format( text[1] )
+
+    return ''.join( text )
 
 
 def sendShortOrLongAnswer( message ):
@@ -341,9 +402,9 @@ def sendShortOrLongAnswer( message ):
     } )
 
     if not response[ 'ok' ]:
-        bot.send_message( message.chat.id, codeHandler( response ) )
+        sendMessage( message.chat.id, codeHandler( response ) )
     else:
-        bot.send_message( message.chat.id, codeHandler( response ) )
+        sendMessage( message.chat.id, codeHandler( response ) )
 
     test( message )
 
@@ -351,6 +412,7 @@ def sendShortOrLongAnswer( message ):
 # Bot default message handler
 @bot.message_handler( content_types = [ 'text' ] )
 def default( message ):
+    # print( message )
     # response = post( 'getStatus', {
     #     'telegramId' : message.chat.id
     # } )
@@ -361,8 +423,10 @@ def default( message ):
     #         return False
     
     # bot.reply_to(message, "Howdy, how are you doing?")
-    bot.send_message( message.chat.id, 'Я вас не понимаю(' )
-        
+    sendMessage( message.chat.id, 'Я вас не понимаю(' )
+    # except telebot.apihelper.ApiException as e:
+        # if 'blocked by user' in e.result:
+            # pass 
 
 
 def checkBot():
@@ -378,7 +442,13 @@ print( 'Run bot with proxy {}:{}'. format(
     connectSettings[ 'proxy' ][ 'port' ]
 ) )
 
-# try:
-bot.polling()
-# except telebot.apihelper.ApiException:
-#     print( e )
+try:
+    bot.polling()
+except telebot.apihelper.ApiException as e:
+    result_dict = json.loads(e.result.text) 
+    description = result_dict['description'] 
+    if description == "bot was blocked by the user": 
+        raise 
+    else: 
+        print("err")
+        pass
